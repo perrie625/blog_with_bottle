96bac326611bf239a3d70dcd09fbb75d5d40d924
perry <q_perry@163.com>
2016-01-22 20:21:12 +0800

    use form

---
+++ favorite_color.tpl
@@ -0,0 +1,11 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <title>Color Selection Confirmation</title>
+</head>
+<body>
+<p>
+	Your favorite color is {{ color }}
+</p>
+</body>
+</html>
--- hello_world.py
+++ hello_world.py
@@ -2,13 +2,20 @@ import bottle
 
 @bottle.route('/')
 def home_page():
-	mythings = ['red', 'blue', 'green', 'purple']
-	return bottle.template('hello_world', username='Perry', things=mythings)
-	# return 'hello_world\n'
+    mythings = ['red', 'blue', 'green', 'purple']
+    return bottle.template('hello_world', username='Perry', things=mythings)
+    # return 'hello_world\n'
 
 @bottle.route('/testpage')
 def test_page():
-	return 'this is a test page'
+    return 'this is a test page'
+
+@bottle.route('/favorite_color', method=["POST", "GET"])
+def favorite_color():
+    color = bottle.request.forms.get('color')
+    if color is None or color == "":
+        color = 'No Color Selected'
+    return bottle.template('favorite_color.tpl', color=color)
 
 bottle.debug(True)
 bottle.run(host='localhost', port=8081)
